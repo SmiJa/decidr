d5fa18811f858290485b21ad6302a0bbf79f9814
Jason Smith <jasons@alphaworks.tech>
2021-11-30 14:39:55 -0800

    feat(display list): display list with function #5

--- src/components/ListDisplay.js
+++ src/components/ListDisplay.js
@@ -2,7 +2,7 @@ export default function ListDisplay(props) {
     const listItems = props.list;
     const buildList = listItems.map((item, index) => {
         return (
-            <li>{item}</li>
+            <li key={index}>{item}</li>
         )
     })
 
