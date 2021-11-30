2b0cde8312790eb7d2dcd392c2917229fa721d68
Jason Smith <jasons@alphaworks.tech>
2021-11-30 15:46:52 -0800

    feat(control): add button to select random item

---
+++ src/components/ChooseItem.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export default function ChooseItem() {
+
+    const randomModal = () => {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+    return (
+        <div id="choose-item">            
+            <button className="choose-btn">Choose Item</button>
+        </div>
+    )
+}
