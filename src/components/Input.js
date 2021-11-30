7d18643f1145507db92ce51f9473d2bb17ac8d50
Jason Smith <jasons@alphaworks.tech>
2021-11-30 14:49:41 -0800

    feat(input item): add controls and function to add item to list #6 #7

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,27 @@
-export default function Input(setListContainer) {
+import { useState } from "react"
+
+export default function Input({listContainer, setListContainer}) {
+    const [newItem, setNewItem] = useState("");
+
+    const formSubmit = (e) => {
+        e.preventDefault();
+        console.log(newItem);
+        setListContainer([...listContainer, newItem]);
+        setNewItem("");
+    }
+
     return (
-        <div>
+        <div id="add-item">
             <form>
-                <div className="input">
-                    <input type="text" />
+                <div className="input-wrap">
+                    <input
+                        type="text"
+                        value={newItem}
+                        onChange={e => setNewItem(e.target.value)}
+                        placeholder="Add an Item"
+                    />
                 </div>                
-                <button className="add-btn">Add Item</button>
+                <button type="submit" className="add-btn" onClick={formSubmit}>Add Item</button>
             </form>
         </div>
     )
