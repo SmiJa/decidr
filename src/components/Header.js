5660d6891940d4ec84581e3863ba7e4c3a40a608
Jason Smith <jasons@alphaworks.tech>
2021-12-03 01:45:58 -0800

    refactor(header): rebuild the header with div tags for style control

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 export default function Header() {
     return (
-        <header>
-            <h1>Decidr</h1>
+        <header id="header">
+            <div className="texture">
+                <div className="texture-2">
+                    <h1>Decidr</h1>
+                </div>
+            </div>
         </header>
     )
 }
