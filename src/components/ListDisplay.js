b9cc3c4276cfa0f7bd48369ace6468bfdf470c0e
Jason Smith <jasons@alphaworks.tech>
2021-12-03 01:51:29 -0800

    chore(object names): rename class and id

--- src/components/ListDisplay.js
+++ src/components/ListDisplay.js
@@ -8,12 +8,12 @@ export default function ListDisplay({listContainer, setListContainer}) {
     const buildList = listContainer.map((item, index) => {
 
         return (
-            <li className="list-item" key={index}>{item} <span onClick={() => removeItemFromList(index)}><i className="bi bi-trash-fill"></i></span></li>
+            <li className="list-item" key={index}>{item} <span onClick={() => removeItemFromList(index)}><i className="bi bi-trash-fill trash"></i></span></li>
         )
     });
 
     return (
-        <div className="list-wrap">
+        <div id="list-wrap">
             <ul className="list">
                 {buildList}
             </ul>
