4953c4e50c0671e66e840c002b7e0c9b01d53cc5
Jason Smith <jasons@alphaworks.tech>
2021-12-03 01:45:29 -0800

    refactor(footer): rebuild the footer with div tags for style control

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 export default function Footer() {
     return (
-        <footer>
-            Built with <span className="heart">&hearts;</span> by Jason Smith
+        <footer id="footer">
+            <div className="texture">
+                <div className="texture-2">
+                    <div className="text-wrap">
+                        Built with <span className="heart">&hearts;</span> by Jason Smith
+                    </div>
+                </div>                
+            </div>            
         </footer>
     )
 }
